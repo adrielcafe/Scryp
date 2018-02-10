af457f9209db7c52fd7d200219532ef42a2fc41a
Adriel Café <me@adriel.cafe>
2018-02-09 21:53:34 -0300

    Restore config file

--- scryp/api.py
+++ scryp/api.py
@@ -1,13 +1,14 @@
 import falcon
 from resources import *
+from config import *
 
 api = falcon.API()
-api.add_route('/', Home())
-api.add_route('/price/{crypto}/{fiat}', Price())
-api.add_route('/balance/{crypto}/{address}', Balance())
-api.add_route('/identify/{address}', Identify())
-api.add_route('/transaction/{crypto}/{address}', Transactions())
-api.add_route('/transaction/{crypto}/single/{txid}', Transactions())
-api.add_route('/block/{crypto}', Block())
-api.add_route('/block/{crypto}/{filter}', Block())
+api.add_route(ROUTE_PREFIX + '/', Home())
+api.add_route(ROUTE_PREFIX + '/price/{crypto}/{fiat}', Price())
+api.add_route(ROUTE_PREFIX + '/balance/{crypto}/{address}', Balance())
+api.add_route(ROUTE_PREFIX + '/identify/{address}', Identify())
+api.add_route(ROUTE_PREFIX + '/transaction/{crypto}/{address}', Transactions())
+api.add_route(ROUTE_PREFIX + '/transaction/{crypto}/single/{txid}', Transactions())
+api.add_route(ROUTE_PREFIX + '/block/{crypto}', Block())
+api.add_route(ROUTE_PREFIX + '/block/{crypto}/{filter}', Block())
 api.add_sink(not_found, '')
